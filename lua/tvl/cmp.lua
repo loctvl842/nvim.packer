8a3bba4defc6b90ccbb48b4bed84e22d25b43113
locletvl842002 <loc.letvl@hcmut.edu.vn>
2022-08-01 21:03:06 +0700

    add highlight first line on insert

--- lua/tvl/cmp.lua
+++ lua/tvl/cmp.lua
@@ -8,30 +8,38 @@ if not snip_status_ok then
 	return
 end
 
-require("luasnip/loaders/from_vscode").lazy_load()
+-- require("luasnip/loaders/from_vscode").lazy_load()
 
 local icons = require("tvl.icons")
 local kind_icons = icons.kind
 
 local check_backspace = function()
-	local col = vim.fn.col(".") - 1
-	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
-
 cmp.setup({
+  completion = {
+    completeopt = 'menu,menuone,noinsert',
+  },
 	snippet = {
 		expand = function(args)
 			luasnip.lsp_expand(args.body) -- For `luasnip` users.
 		end,
 	},
 	mapping = {
-		["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+		["<C-j>"] = cmp.mapping(function(fallback) 
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { "i", "c" }),
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
 		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 		["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-		["<C-e>"] = cmp.mapping({
+		["<C-c>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
 		}),
@@ -41,10 +49,12 @@ cmp.setup({
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif luasnip.expandable() then
-				luasnip.expand()
+			elseif luasnip.jumpable(1) then
+				luasnip.jump(1)
 			elseif luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
+			elseif luasnip.expandable() then
+				luasnip.expand()
 			elseif check_backspace() then
 				fallback()
 			else
@@ -92,7 +102,7 @@ cmp.setup({
 	sources = {
 		{ name = "nvim_lsp" },
 		{ name = "nvim_lua" },
-		{ name = "luasnip" },
+		{ name = "luasnip", option = { use_show_condition = false } },
 		{ name = "buffer" },
 		{ name = "path" },
 	},
@@ -103,18 +113,17 @@ cmp.setup({
 	window = {
 		-- documentation = "native",
 		documentation = {
-			border = "single",
+			border = "rounded",
 			-- border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
 			winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
 		},
 		completion = {
-			border = "single",
+			border = "rounded",
 			-- border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
 			winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
 		},
 	},
 	experimental = {
-		ghost_text = false,
-		native_menu = false,
+		ghost_text = true,
 	},
 })
