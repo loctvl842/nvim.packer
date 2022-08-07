3f56a6da2ebba9f864b1dcc83df6da634c78d283
locletvl842002 <loc.letvl@hcmut.edu.vn>
2022-08-07 22:14:51 +0700

    this commit id of nvim is fine

--- install_neovim.sh
+++ install_neovim.sh
@@ -2,12 +2,14 @@
 
 git clone https://github.com/neovim/neovim.git ~/.config/nvim/neovim
 
+cd ~/.config/nvim/neovim
+
+git reset --hard d15a66d80336ff3f6c69a0eff4c17c83a84d44c9
+
 sudo rm /usr/local/bin/nvim
 sudo rm -r /usr/local/share/nvim/
 
-cd ~/.config/nvim/neovim
-
-make CMAKE_BUILD_TYPE=Release
+make CMAKE_BUILD_TYPE=RelWithDebInfo
 
 sudo make install
 
